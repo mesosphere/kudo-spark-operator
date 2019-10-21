4ff9193864d8b66acf49710b2bac1016a7f97b2d
Roman Palaznik <rpalaznik.c@mesosphere.io>
2019-10-21 15:00:59 +0300

    DCOS-60183: Use 2tvenom/go-test-teamcity when running tests in Teamcity

--- tests/run.sh
+++ tests/run.sh
@@ -9,5 +9,17 @@ set -e
 # - SPARK_VERSION
 # Default values can be looked up in tests/utils/common.go
 pushd $TEST_DIR
-go test -v -count=1 -timeout 30m .
+
+if [ -n "$TEAMCITY_VERSION" ]; then
+  # Running in Teamcity, therefore we need to convert `go test` output accordingly
+  if command -v go-test-teamcity; then
+    go test -v -count=1 -timeout 30m . | go-test-teamcity
+  else
+    go test -v -count=1 -timeout 30m . | docker run -i xjewer/go-test-teamcity
+  fi
+else
+  go test -v -count=1 -timeout 30m .
+fi
+
+
 popd
