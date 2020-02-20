1b89760910d57ab2c2f31edc9cddf1c663491078
Alex Lembiewski <alembiyeuski.c@d2iq.com>
2020-02-20 15:24:41 +0300

    Address code review comments, code refactored
    Signed-off-by: Alex Lembiewski <alembiyeuski.c@d2iq.com>

--- tests/hdfs_kerberos_test.go
+++ tests/hdfs_kerberos_test.go
@@ -8,6 +8,10 @@ import (
 	"time"
 )
 
+// note: this shouldn't be changed as per this section:
+//https://github.com/mesosphere/spark-on-k8s-operator/blob/master/docs/user-guide.md#mounting-secrets
+const hadoopTokenFileName = "hadoop.token"
+
 var (
 	resourceFolder         = "resources/hdfs-kerberos"
 	namespace              = "hdfs-kerberos"
@@ -38,6 +42,8 @@ var (
 	}
 	eventLogDir       = "hdfs://namenode.hdfs-kerberos.svc.cluster.local:9000/history"
 	hadoopTokenSecret = "hadoop-token"
+	hadoopTokenPath   = fmt.Sprint("/tmp/", hadoopTokenFileName)
+	waitTimeout       = 5 * time.Minute
 )
 
 type Resource struct {
@@ -56,33 +62,47 @@ func TestHdfsIntegrationSuite(t *testing.T) {
 }
 
 func (suite *HdfsIntegrationSuite) SetupSuite() {
-	utils.Kubectl("create", "ns", namespace)
+	if _, err := utils.Kubectl("create", "ns", namespace); err != nil {
+		suite.FailNow("Error while creating namespace", err)
+	}
 	// deploy KDC and HDFS
 	for _, resource := range hdfsKerberosDeployment {
-		utils.Kubectl("apply", "-f", fmt.Sprint(resourceFolder, "/", resource.path), "-n", namespace)
-
+		if _, err := utils.Kubectl("apply", "-f", fmt.Sprint(resourceFolder, "/", resource.path), "-n", namespace); err != nil {
+			suite.FailNowf("Error while creating \"%s\"", resource.name, err)
+		}
 		if resource.wait {
-			utils.Kubectl("wait", fmt.Sprint("deployments/", resource.name),
-				"--for=condition=available", "--timeout=3m", "-n", namespace)
+			if _, err := utils.Kubectl("wait", fmt.Sprint("deployments/", resource.name),
+				"--for=condition=available",
+				fmt.Sprintf("--timeout=%v", waitTimeout),
+				"-n", namespace); err != nil {
+				suite.FailNowf("Error while waiting for resource \"%s\" to be deployed", resource.name, err)
+			}
 		}
 	}
-	nameNodePod, err := utils.Kubectl("get", "pods", "--selector=name=hdfs-namenode",
-		"--output=jsonpath={.items[*].metadata.name}", "-n", namespace)
+	// get the name of a Namenode pod
+	nameNodePod, err := utils.Kubectl("get", "pods",
+		"--selector=name=hdfs-namenode", "--output=jsonpath={.items[*].metadata.name}", "-n", namespace)
 	if err != nil {
-		suite.FailNow("Error while getting a name of Namenode pod", err)
+		suite.FailNow("Error while getting Namenode pod name", err)
 	}
 	// run init script to copy test data to HDFS and export delegation token
-	utils.Kubectl("exec", nameNodePod, "-n", namespace, "--", "init.sh")
+	if _, err := utils.Kubectl("exec", nameNodePod, "-n", namespace, "--", "init.sh"); err != nil {
+		suite.FailNow("Error while running initialization script", err)
+	}
 
 	// copy delegation token from the pod to a local filesystem
-	utils.Kubectl("cp", fmt.Sprint(nameNodePod, ":", "tmp/hadoop.token"), "/tmp/hadoop.token", "-n", namespace)
-	utils.Kubectl("create", "secret", "generic", hadoopTokenSecret, "--from-file", "/tmp/hadoop.token", "-n", namespace)
+	if _, err := utils.Kubectl("cp", fmt.Sprint(nameNodePod, ":", hadoopTokenPath[1:]),
+		hadoopTokenPath, "-n", namespace); err != nil {
+		suite.FailNow("Error while copying the delegation token", err)
+	}
+	if _, err := utils.Kubectl("create", "secret",
+		"generic", hadoopTokenSecret, "--from-file", hadoopTokenPath, "-n", namespace); err != nil {
+		suite.FailNow("Error while creating a secret for delegation token", err)
+	}
 }
 
 // invoked before each test
 func (suite *HdfsIntegrationSuite) BeforeTest(suiteName, testName string) {
-	fmt.Println(suiteName, testName)
-
 	suite.operator = utils.SparkOperatorInstallation{
 		Namespace:            namespace,
 		SkipNamespaceCleanUp: true,
@@ -101,24 +121,22 @@ func (suite *HdfsIntegrationSuite) BeforeTest(suiteName, testName string) {
 }
 
 func (suite *HdfsIntegrationSuite) AfterTest(suiteName, testName string) {
-	utils.Kubectl("kudo", "uninstall",
-		"--instance", suite.operator.InstanceName,
-		"--namespace", suite.operator.Namespace)
-	// TODO: temporary workaround: explicitly waiting for uninstall to clean all the resources before the next test starts.
-	time.Sleep(15 * time.Second)
+	// remove the operator and wait until all resources are deleted
+	utils.Kubectl("delete", "instance.kudo.dev", suite.operator.InstanceName,
+		"-n", suite.operator.Namespace, "--wait=true")
 }
 
 func (suite *HdfsIntegrationSuite) Test_Spark_Hdfs_Kerberos() {
 	jobName := "spark-hdfs-kerberos"
-	sparkPi := utils.SparkJob{
+	sparkJob := utils.SparkJob{
 		Name:     jobName,
 		Template: fmt.Sprintf("%s.yaml", jobName),
 	}
-	if err := suite.operator.SubmitJob(&sparkPi); err != nil {
+	if err := suite.operator.SubmitJob(&sparkJob); err != nil {
 		suite.FailNow(err.Error())
 	}
 
-	if err := suite.operator.WaitUntilSucceeded(sparkPi); err != nil {
+	if err := suite.operator.WaitUntilSucceeded(sparkJob); err != nil {
 		suite.FailNow(err.Error())
 	}
 }
@@ -142,7 +160,21 @@ func (suite *HdfsIntegrationSuite) Test_Spark_Hdfs_Kerberos_SparkHistoryServer()
 	if err := suite.operator.WaitUntilSucceeded(sparkJob); err != nil {
 		suite.FailNow(err.Error())
 	}
-	// TODO: verify the job appears in SHS logs
+
+	// check the logs to verify the app has been parsed by Spark History Server
+	historyServerPodName, _ := utils.Kubectl("get", "pods", "--namespace", suite.operator.Namespace,
+		"--selector=app.kubernetes.io/name=spark-history-server", "--output=jsonpath={.items[*].metadata.name}")
+
+	logRecord := fmt.Sprintf("FsHistoryProvider: Finished parsing %s/spark-", eventLogDir)
+	utils.Retry(func() error {
+		contains, err := utils.PodLogContains(suite.operator.Namespace, historyServerPodName, logRecord)
+		if err != nil {
+			return err
+		} else if !contains {
+			return fmt.Errorf("text is not present in the logs, retrying")
+		}
+		return nil
+	})
 }
 
 func (suite *HdfsIntegrationSuite) TearDownSuite() {
