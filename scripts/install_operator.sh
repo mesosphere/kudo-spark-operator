2b0647c74894635996df1dd536eb36550961e328
Aleksei Gorobets <agorobets@d2iq.com>
2021-03-17 10:08:22 +0100

    add docs

--- scripts/install_operator.sh
+++ scripts/install_operator.sh
@@ -2,7 +2,7 @@
 
 set -ex
 SCRIPT_DIR=$(dirname "$0")
-OPERATOR_DIR="$(dirname ${SCRIPT_DIR})/operator"
+OPERATOR_DIR="$(dirname ${SCRIPT_DIR})/kudo-spark-operator/operator"
 
 NAMESPACE=${NAMESPACE:-spark}
 OPERATOR_DOCKER_REPO=${OPERATOR_DOCKER_REPO:-mesosphere/kudo-spark-operator}
